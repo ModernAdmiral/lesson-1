9b2cd9766afe68c849b947202e58ad39309178fe
Jackson McComas <jackson.mccomas@gmail.com>
2019-11-28 15:15:47 -0600

    displayed shop data to page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import HomePage from "./Pages/Homepage/Homepage.jsx";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from "./Pages/Shop/shop.jsx";
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
         {/* <Route path="/jackets" component={JacketsPage} />
         <Route path="/sneakers" component={SneakersPage} />
         <Route path="/womens" component={WomensPage} />
