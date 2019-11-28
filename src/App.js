efb9599b6e358a7bcdead49807f2cafd61419db9
Jackson McComas <jackson.mccomas@gmail.com>
2019-11-27 22:29:01 -0600

    completed some basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./Pages/Homepage/Homepage.jsx";
+import { Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
-}
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+        {/* <Route path="/jackets" component={JacketsPage} />
+        <Route path="/sneakers" component={SneakersPage} />
+        <Route path="/womens" component={WomensPage} />
+        <Route path="/mens" component={MensPage} /> */}
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
